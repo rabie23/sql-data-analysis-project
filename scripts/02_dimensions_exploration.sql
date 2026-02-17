0d0344c24568b2bbd02716630866c0f352334bac
rabie23 <115818018+rabie23@users.noreply.github.com>
2026-02-17 09:02:16 +0100

    Update 02_dimensions_exploration.sql

--- scripts/02_dimensions_exploration.sql
+++ scripts/02_dimensions_exploration.sql
@@ -1,1 +1,26 @@
-test
+/*
+===============================================================================
+Dimensions Exploration
+===============================================================================
+Zweck:
+Untersuchung der Struktur von Dimensionstabellen.
+	
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+===============================================================================
+*/
+
+-- Retrieve a list of unique countries from which customers originate
+SELECT DISTINCT 
+    country 
+FROM gold.dim_customers
+ORDER BY country;
+
+-- Retrieve a list of unique categories, subcategories, and products
+SELECT DISTINCT 
+    category, 
+    subcategory, 
+    product_name 
+FROM gold.dim_products
+ORDER BY category, subcategory, product_name;
